bd21257148a4061043980b75ba54d8006e3c7433
CeilingBug <Tiantiandunn@gmail.com>
2024-12-14 12:20:06 -0500

    Update hospitalFunding.py

--- hospitalFunding.py
+++ hospitalFunding.py
@@ -6,9 +6,9 @@ page = requests.get("https://en.wikipedia.org/wiki/List_of_hospitals_in_Canada")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # create object
-object = soup.find(class_="vector-toc-link")
+#object = soup.find(class_="vector-toc-link")
 
-result = items[0]
+result = soup.find_all('p')
 
 print(result.prettify())
 
