49163764e7827b9d0a1f7b1ea1ae4508d87f102d
CeilingBug <Tiantiandunn@gmail.com>
2024-12-14 11:25:32 -0500

    Update hospitalFunding.py
    using beautiful soup

--- hospitalFunding.py
+++ hospitalFunding.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
 
+page = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+ 
+soup = BeautifulSoup(page.content, 'html.parser')
+ 
+# create object
+object = soup.find(id="mp-left")
+ 
+items = object.find_all(class_="mp-h2")
+result = items[0]
+ 
+print(result.prettify())
+
+hospitalThreeStepsCanada = {
+  
+}
