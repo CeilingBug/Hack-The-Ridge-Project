4ee61f91c465de9ee2a8a76fdc3204059a24f70d
CeilingBug <Tiantiandunn@gmail.com>
2024-12-14 11:57:15 -0500

    Update hospitalFunding.py

--- hospitalFunding.py
+++ hospitalFunding.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get("https://en.wikipedia.org/wiki/Main_Page")
- 
+page = requests.get("https://en.wikipedia.org/wiki/List_of_hospitals_in_Canada")
+
 soup = BeautifulSoup(page.content, 'html.parser')
- 
+
 # create object
 object = soup.find(id="mp-left")
- 
+
 items = object.find_all(class_="mp-h2")
 result = items[0]
- 
+
 print(result.prettify())
 
 hospitalThreeStepsCanada = {
