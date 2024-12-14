dd8d274790fe587c0850c08152b7fe2b15e14e03
CeilingBug <Tiantiandunn@gmail.com>
2024-12-14 12:38:34 -0500

    Update hospitalFunding.py

--- hospitalFunding.py
+++ hospitalFunding.py
@@ -1,17 +1,25 @@
-from bs4 import BeautifulSoup
-import requests
-
-page = requests.get("https://en.wikipedia.org/wiki/List_of_hospitals_in_Canada")
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# create object
-#object = soup.find(class_="vector-toc-link")
-
-result = soup.find_all('p')
-
-print(result.prettify())
+quebecList = []
+ontarioList = []
+albertaList = []
+nlList = []
+novaList = []
+brunsList = []
+saskList = []
+bcList = []
+yukonList = []
+nunavutList = []
+nwtList = []
 
 hospitalThreeStepsCanada = {
-  
+  "Ontario" : ontarioList,
+  "Quebec" : quebecList,
+  "Alberta" : albertaList,
+  "Newfoundland and Labrador" : nlList,
+  "Nova Socia" : novaList,
+  "New Brunswick" : brunsList,
+  "Saskatchawan" : saskList,
+  "British Columbia" : bcList,
+  "Yukon" : yukonList,
+  "Nunavut" : nunavutList,
+  "North West Territories" : nwtList
 }
